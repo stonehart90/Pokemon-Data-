7adef44b177594a3bd9ed989aa11a03b8c20d988
stonehart90 <43418849+stonehart90@users.noreply.github.com>
2018-12-16 20:40:17 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -1,5 +1,3 @@
-
-
 library(shiny)
 library(tidyverse)
 library(datasets)
@@ -11,11 +9,11 @@ ui <- fluidPage(
   
   # Application title
   titlePanel("Pokemon Statistics"),
-  textAreaInput("caption", "Beta Application Background", "In this turn based game, Pokemon with the highest speed get to attack first, allowing for a swift advantage in battle. Pokemon with higher attack deal more damage with physical moves, while pokemon with higher special attack deal more damage with non-physical moves. Similar logic applies to defense and special defense, only they serve to help prevent a Pokemon's Hit Points (HP) from reaching zero and fainting. Do pay attention to outliers on the right. They often serve as an indication of a Legendary Pokemon, which are typically banned from competitve play. ", width = "1000px"),
+  textAreaInput("caption", "Application Background", "In this turn based game, Pokemon with the highest speed get to attack first, allowing for a swift advantage in battle. Pokemon with higher attack deal more damage with physical moves, while pokemon with higher special attack deal more damage with non-physical moves. Similar logic applies to defense and special defense, only they serve to help prevent a Pokemon's Hit Points (HP) from reaching zero and fainting. Do pay attention to outliers on the right. They often serve as an indication of a Legendary Pokemon, which are typically banned from competitve play. ", width = "1000px", height = "100px"),
   textAreaInput("caption", "Data Source", "These data come straight from publicly available Pokemon base stat totals."),
   textAreaInput("caption", "GitHub Repo", "https://github.com/stonehart90/Pokemon-Data-"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a slider input for statistical category 
   sidebarLayout(
     sidebarPanel(
       selectInput(inputId = "x", 
@@ -31,14 +29,15 @@ ui <- fluidPage(
                               "5",
                               "6",
                               "7")),
-      textInput("name","Enter a Pokemon Name (Coming Soon)",""),
+      textInput("name","Enter a Pokemon Name to See its Stat in Chosen Category (Case Sensitive):",""),
       mainPanel()
       
     ),
-    #Above I devide the pokemon by generation so that players can better divide statistical distributions. 
+    #Above I divide the pokemon by generation so that players can better divide statistical distributions. 
     #I also use "choices" to allow the stats I want to analyze to be displayed in the drop down menu.
     # Show a plot of the generated distribution
     mainPanel(
+      
       tabsetPanel(type = "tab",
                   tabPanel("Pokemon by the Numbers",plotOutput("generationPlot")),
                   tabPanel("Pokemon Search", plotOutput("namePlot"))
@@ -49,7 +48,6 @@ ui <- fluidPage(
 
 #Define server logic required to draw a histogram
 
-
 server <- function(input, output) {
   
   output$generationPlot <- renderPlot({
@@ -57,24 +55,24 @@ server <- function(input, output) {
     pokemon_stats %>% 
       filter(generation == input$generation) %>% 
       
-
       ggplot(aes_string(x = input$x)) +
-      geom_histogram(binwidth = .5, na.rm = TRUE, stat = "count") + 
+      geom_histogram(color = "black", fill = "yellow", binwidth = .5, na.rm = TRUE, stat = "count") + 
       labs(aes_string(x = input$x),
            title = "Pokemon Stat Spread Count by Generation",
            caption = "Pokemon Stat Spread Count by Generation") 
     
-    # draw the histogram with the specified number of bins
+    # draws the histogram to show count
     
   })
   
   output$namePlot <- renderPlot({
-  
-  what <- pokemon_stats 
-    filter(name == input$name) %>% 
-     select(speed, attack, defense, hp, sp_attack, sp_defense)
-      ggplot(what,aes(x = pokemon_stats, y = count) + geom_bar())
-    #Here I am developing a way to search for a specific Pokemon. This is a bonus feature for the most serious players. 
+    #The following code allows a user to make a barplot for a Pokemon to see its specific stat. Notice the combintion of both aes and aes_string
+    pokemon_stats %>% 
+      filter(name == input$name) %>% 
+      group_by(name) %>% 
+     ggplot(aes(x = name, y = density(count))) + aes_string(y = input$x) + geom_col(color = "Black", fill = "Pink", binwidth =.05)
+                        
+
   })
   
 }
